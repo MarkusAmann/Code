72497c43c462a2dabb5bca124148b0422c1ef618
Markus Amann <markus.2504@yahoo.de>
2022-05-09 15:04:18 +0200

    Bugfix adjungierte DGL Kreisfahrt

--- Kreisfahrt/Stellgroesse_Ruck_dkappa_Bestrafung_von_dr/simulation.m
+++ Kreisfahrt/Stellgroesse_Ruck_dkappa_Bestrafung_von_dr/simulation.m
@@ -5,7 +5,7 @@ clear all
 %% Parameter
 jlim = 1.06*1000; dkappalim = 1/4*1000; use_umax = 0; use_dr = 1;
 umax = [jlim;dkappalim]; umin = -[jlim;dkappalim];
-t0 = 0; tf = 1; N = 100; fjy = 1; fjx = 1; fax = 1; fay = 1; fr = 0.01; kapparef = 0.01; sf = 1000; drf = 0; psirf = 0;
+t0 = 0; tf = 1; N = 100; fjy = 1; fjx = 1; fax = 1; fay = 1; fr = 1; kapparef = 0.0; sf = 1000; drf = 0; psirf = 0;
 x0 = [0 5 0 0 0 kapparef].'; l0 = [0 0 0 0 0 0].'; %l0 = 0.1*randn(4,1);
 
 p.use_umax = use_umax; p.umax = umax; p.umin = umin; p.use_dr = use_dr; p.fjx = fjx; p.fjy = fjy; p.fax = fax; p.fay = fay; p.fr = fr; p.kapparef = kapparef; p.sf = sf; p.drf = drf; p.psirf = psirf;
@@ -13,17 +13,17 @@ p.x0 = x0; p.l0 = l0; p.t0 = t0; p.tf = tf; p.N = N;
 
 
 %% Ruhelage der Kreisfahrt mit Bestrafung von dr und 1-dr*kappar = 1
-syms vsym
-v_RL_all = solve(1/2*p.fay^2/p.fr*p.kapparef^6*vsym^8-3/2*p.fay*kapparef^2*vsym^4+1 == 0, vsym);
-v_RL_all = double(v_RL_all);
-v_RL = min(v_RL_all(imag(v_RL_all) == 0 & real(v_RL_all) >= 0));
-v_RL_approx_auf_ref = (2/(3*p.fay*p.kapparef^2))^(1/4);
-dr_RL = p.kapparef^3*v_RL^4*p.fay/p.fr;
-l1_RL = -2*p.fay*p.kapparef^2*v_RL^3;
-l5_RL = -p.fay*p.kapparef*v_RL^3;
-kappa_RL = p.kapparef;
-kappa_RL_corrected = p.kapparef/(1-dr_RL*p.kapparef);
-ay_RL = kappa_RL_corrected*v_RL^2;
+% syms vsym
+% v_RL_all = solve(1/2*p.fay^2/p.fr*p.kapparef^6*vsym^8-3/2*p.fay*kapparef^2*vsym^4+1 == 0, vsym);
+% v_RL_all = double(v_RL_all);
+% v_RL = min(v_RL_all(imag(v_RL_all) == 0 & real(v_RL_all) >= 0));
+% v_RL_approx_auf_ref = (2/(3*p.fay*p.kapparef^2))^(1/4);
+% dr_RL = p.kapparef^3*v_RL^4*p.fay/p.fr;
+% l1_RL = -2*p.fay*p.kapparef^2*v_RL^3;
+% l5_RL = -p.fay*p.kapparef*v_RL^3;
+% kappa_RL = p.kapparef;
+% kappa_RL_corrected = p.kapparef/(1-dr_RL*p.kapparef);
+% ay_RL = kappa_RL_corrected*v_RL^2;
 %p.x0 = [0 v_RL 0 dr_RL 0].'; p.l0 = [l1_RL 0 0 0 l5_RL].';
 
 %% Optimierung
@@ -35,6 +35,8 @@ p.deltat = deltat;
 init_guess = @(x)guess_free_tf(t,p);
 start_inits = [10];
 inits = start_inits;
+init_order = floor((log10(start_inits)>0).*log10(start_inits));
+inits = 10.^(floor((log10(start_inits)>0).*log10(start_inits))).*abs(randn(size(start_inits))).*sign(start_inits);
 error_flag = 1;
 while error_flag
     try
@@ -68,7 +70,7 @@ l4opt = sol.y(10,:);
 l5opt = sol.y(11,:);
 l6opt = sol.y(12,:);
 delta_opt = sol.parameters;
-tf_opt = delta_opt*p.tf;
+tf_opt = delta_opt*p.tf
 sol_mesh = sol_mesh*delta_opt;
 
 %%
@@ -78,8 +80,11 @@ for i=1:length(sol_mesh)
 end
 jopt = u(1,:);
 dkappaopt = u(2,:);
-% kappaopt(1) = interp1(t(2:end),kappaopt(2:end),0,'pchip','extrap');
+jyopt = dkappaopt.*vopt.^2 + 2*kappaopt.*aopt.*vopt;
+J_fun = 1/2*p.fjx*jopt.^2 + 1/2*p.fax*aopt.^2 + 1/2*p.fjy*jyopt.^2 + 1/2*p.fay*kappaopt.^2.*vopt.^4 + 1/2*p.fr*dropt.^2*p.use_dr;
+J = trapz(sol_mesh,J_fun) + tf_opt 
 
+%%
 % time-derivatives of optimal values and values of reference curve
 dot_sopt = vopt.*cos(psiropt)./(1-dropt*kapparef);
 dot_psi = kappaopt.*vopt;
